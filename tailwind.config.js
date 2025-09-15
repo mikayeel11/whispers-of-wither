9e98dff8fce3ea663248ef9e1db29fb49c167d4f
mikayeel11 <mikimirza786@gmail.com>
2025-09-14 23:27:04 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      fontFamily: {
-        mono: ['IBM Plex Mono', 'monospace'],
-        serif: ['Georgia', 'serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
